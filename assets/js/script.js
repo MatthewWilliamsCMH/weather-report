5ff6b9da2be349f9057c68e9d33c6e1b9f995c4c
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-06-04 15:53:10 -0400

    data reports to one-day card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,103 @@
 const searchButton = document.querySelector("#searchButton");
 const searchList = [];
-let oneDay = "";
-let fiveDay = "";
-let cityLat = "";
-let cityLon = "";
+let searchCity=""
+let oneDay = [];
+let fiveDay = [];
+// let cityLat = "";
+// let cityLon = "";
 
-//can we remove this and just put coords into cityLatLon?
-function getForecast(searchCity) {
-  const coords = cityLatLon(searchCity)
-}
+//can we remove this and just put coords into getLatLon?
+// function getForecast(searchCity) {
+//   const coords = getLatLon(searchCity)
+// }
 
-function cityLatLon(searchCity) {
+function getLatLon(searchCity) {
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&appid=671277334815afdc86042e04b061da17`, {
   })
   .then(function (response) {
     return response.json();
   })
   .then(function(data) {
-    cityLat = data[0].lat;
-    cityLon = data[0].lon;
-    getWeather(cityLat, cityLon)
+    const cityLat = data[0].lat;
+    const cityLon = data[0].lon;
+    getWeather(cityLat, cityLon);
   })
   .catch(function(error) {
     console.error('Error fetching data:', error);
-  })
+  });
 }
 
 function getWeather(lat, lon){
   fetch(`https:api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&mode=json&units=imperial&appid=671277334815afdc86042e04b061da17`, {
-  })
+  });
   .then (function(response) {
     return response.json();
-  })
+  });
   .then (function(data) {
-    //do i need this .then at all?
-    console.log(data)
-  })
+    oneDay = data.list[0];
+    createOneDayCard(oneDay);
+  });
   .catch (function (error) {
     console.error("Error ", error);
-  })
-  }
+  });
   
+}
+
+function createOneDayCard() {
+  const searchDate = findDate();
+  
+  const resultCard = document.createElement("oneDayCardEl");
+  resultCard.classList.add("one-day-card");
+
+  const cardTitle = document.createElement("h2");
+  cardTitle.textContent = `${searchCity} (${searchDate}) [icon]`;
+  resultCard.append(cardTitle);
+
+  const cardText = document.createElement("ul");
+  cardText.textContent = "<li>Temp: " + oneDay.main.temp + "</li>\n" +
+  "<li>Humidity: " + oneDay.main.humidity + "</li>\n" +
+  "<li>Wind: " + oneDay.wind.speed + "</li>"
+  
+resultCard.append(cardText);
+oneDayCardEl.append(resultCard);
+
+  console.log(oneDay);
+  console.log(searchCity);
+}
+
+function findDate() {
+  const date = new Date();
+  const currentYear = String(date.getFullYear());
+  const currentMonth = String(date.getMonth() + 1);
+  const currentDay = String(date.getDate());
+  curDate = `${currentMonth}/${currentDay}/${currentYear}`;
+  return curDate;
+}
+// function createResultCard(resultItem) {
+//   const resultCard = document.createElement('div');
+//   resultCard.classList.add('result-card');
+
+//   const cardTitle = document.createElement('h3');
+//   cardTitle.textContent = resultItem.title;
+
+//   resultCard.append(cardTitle)
+
+//   const cardText = document.createElement('p');
+//   cardText.textContent = resultItem.description;
+
+//   resultCard.append(cardText);
+
+//   resultBox.append(resultCard);
+// }
+
 // should I also look for state? what to do if state field empty?
-
-
-// const searchButton = document.querySelector("#searchButton");
-// const searchList = [];
-// let oneDay = "";
-// let fiveDay = "";
-// let myLat = "";
-// let myLon = "";
-
-// async function getForecast(searchCity) {
-//   await myCoords(searchCity)
-//   const response = fetch(`https:api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLon}&cnt=5&mode=json&units=imperial&appid=671277334815afdc86042e04b061da17`, {
-//   })
-//   .then(function (response) {
-//     return response.json()
-//   })
-//   .then(function (data) {
-//   // .catch
-//   //error trapping
-//   })
-// // };
-// };
-
-// async function myCoords(searchCity) {
-//   return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&appid=671277334815afdc86042e04b061da17`, {
-//   })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     myLat = data[0].lat;
-//     myLon = data[0].lon;
-//     return {myLat, myLon}
-//   // .catch
-//   // need to catch typos
-//   // should I also look for state? what to do if state field empty?
-//   })
-// };
 
 searchButton.addEventListener("click", function(event) {
   event.preventDefault()
   // test below for content other than letter, period, hyphen.
-  const searchCity = document.querySelector("#searchInput").value;
+  searchCity = document.querySelector("#searchInput").value;
   if (searchCity.trim()!== ""){ 
-    getForecast(searchCity);
+    getLatLon(searchCity);
   } else {
     alert("Please provide a city name.")
     return;
