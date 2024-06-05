b4fd1b0f0d08a35785fad09c240cd9f530e5cdb4
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-06-05 01:29:59 -0400

    refactoring to handle all card making in one function; working on filtering results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
+// get date
+// get lat and lon
+// get weather
+// write city name and fetch http address to the end of localStorage
+// filter weather to index[0] plus all the noon readings for dates after today
+// create card 
+
 const searchButton = document.querySelector("#searchButton");
 const dataList = document.querySelector("#oneDayCardEl");
-
-const searchList = [];
+const fullDate = new Date();
+let curDate = ""
 let searchCity=""
 let oneDay = [];
 let fiveDays = [];
+
+
+// read city name and fetch http addresses from local Storage
+// display city name in left column on button that pulls the fetch request into the createCards routine
+const searchList = [];
+
+function getToday (fullDate) {
+  const month = (fullDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = fullDate.getDate().toString().padStart(2, '0');
+  const year = fullDate.getFullYear();
+  curDate = `${month}/${day}/${year}`;
+}
 
 function getLatLon(searchCity) {
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&appid=671277334815afdc86042e04b061da17`, {
@@ -23,7 +42,7 @@ function getLatLon(searchCity) {
 }
 
 function getWeather(lat, lon){
-  fetch(`https:api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&mode=json&units=imperial&appid=671277334815afdc86042e04b061da17`, {
+  fetch(`https:api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=40&mode=json&units=imperial&appid=671277334815afdc86042e04b061da17`, {
   })
   .then (function(response) {
     return response.json();
@@ -39,12 +58,43 @@ function getWeather(lat, lon){
 
   // in createFiveDaysCards function, iterate over the remaining array
   // to create four new cards
+    trimWeather(data)
     createOneDayCard(oneDay)
     createFiveDayCards(fiveDays)
   })
   .catch (function (error) {
     console.error("Error ", error);
   })
+}
+function trimWeather(data) {
+// compare ONLY the time in the data.list[i].dt_txt to see if it's 00:00:00
+// use the .filter function to remove data.list[i] if the result is false
+// compare ONLY the date in fullDate with ONLY the date in data.list[i].dt_txt;
+
+
+  // console.log(data)
+
+  for (let i = 0; i < data.list.length; i++) {
+    let checkTime = data.list[i].dt_txt.slice(-8); // return the last 8 characters, which should be the time in 00:00:00 format
+    if (checkTime !== "00:00:00") {
+      console.log("true")
+      console.log(i)
+      console.log(data)
+      delete data.list[i]
+      console.log("hello")
+      console.log(data)
+    }
+    // console.log(checkDate)
+    // console.log(fullDate)
+    // console.log(data.list[i].filter(checkDate !== fullDate.toTimeString()));
+  }
+  // const result = data.filter(item => item.list.dt_text.toTimeString() !== fullDate.toTimeString());
+  // console.log(result)
+//   for(let i = 0; i < data.length; i++)
+//     if (checkDate === fullDate) {
+//       if (checkDate .toTimeString() !== fullDate.toTimeString())
+//         data[i]
+//     }
 }
 
 function createOneDayCard() {
@@ -69,8 +119,6 @@ function createOneDayCard() {
   const cityWind = document.createElement("li");
   cityWind.textContent = "Wind: " + oneDay.wind.speed + " mph";
   
-  console.log(oneDay)
-
   resultCard.appendChild(cityTemp)
   resultCard.appendChild(cityHumidity)
   resultCard.appendChild(cityWind)
